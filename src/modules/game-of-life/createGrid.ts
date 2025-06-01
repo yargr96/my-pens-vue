9ea3f88fdcf9650e45fe0092c52f8c736b5ee49c
Yaroslav Gromov <yaroslav.gromov@1win.pro>
2025-06-01 23:04:01 +0700

    lint

--- src/modules/game-of-life/createGrid.ts
+++ src/modules/game-of-life/createGrid.ts
@@ -1,164 +1,154 @@
 import { subtractVector, type Vector } from '@/utils/vector';
 
 type GridColors = {
-    colorBackground: string;
-    colorGrid: string;
-    colorCell: string;
-}
+  colorBackground: string;
+  colorGrid: string;
+  colorCell: string;
+};
 
 type GridParams = {
-    canvas: HTMLCanvasElement;
-    context: CanvasRenderingContext2D;
-    cellSize: number;
-    showGrid: boolean;
-    colors: GridColors;
-}
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
+  cellSize: number;
+  showGrid: boolean;
+  colors: GridColors;
+};
 
 export type GridSize = {
-    xCellsCount: number;
-    yCellsCount: number;
-}
+  xCellsCount: number;
+  yCellsCount: number;
+};
 
 type GridSizeParams = GridSize & {
-    gridWidth: number;
-    gridHeight: number;
-    offsetLeft: number;
-    offsetTop: number;
-}
+  gridWidth: number;
+  gridHeight: number;
+  offsetLeft: number;
+  offsetTop: number;
+};
 
 type GetCellByCoordinates = (coordinates: Vector) => Vector | undefined;
 
 export type Grid = {
-    renderGrid: () => void;
-    renderCell: (cell: Vector) => void;
-    gridSizeParams: GridSizeParams;
-    getCellByCoordinates: GetCellByCoordinates;
-}
+  renderGrid: () => void;
+  renderCell: (cell: Vector) => void;
+  gridSizeParams: GridSizeParams;
+  getCellByCoordinates: GetCellByCoordinates;
+};
 
 const getGridSizeParams = (canvas: HTMLCanvasElement, cellSize: number): GridSizeParams => {
-    const xCellsCount = Math.floor(canvas.width / cellSize);
-    const yCellsCount = Math.floor(canvas.height / cellSize);
+  const xCellsCount = Math.floor(canvas.width / cellSize);
+  const yCellsCount = Math.floor(canvas.height / cellSize);
 
-    const gridWidth = cellSize * xCellsCount;
-    const gridHeight = cellSize * yCellsCount;
+  const gridWidth = cellSize * xCellsCount;
+  const gridHeight = cellSize * yCellsCount;
 
-    const offsetLeft = (canvas.width - gridWidth) / 2;
-    const offsetTop = (canvas.height - gridHeight) / 2;
+  const offsetLeft = (canvas.width - gridWidth) / 2;
+  const offsetTop = (canvas.height - gridHeight) / 2;
 
-    return {
-        xCellsCount,
-        yCellsCount,
-        gridWidth,
-        gridHeight,
-        offsetLeft,
-        offsetTop,
-    };
+  return {
+    xCellsCount,
+    yCellsCount,
+    gridWidth,
+    gridHeight,
+    offsetLeft,
+    offsetTop,
+  };
 };
 
 const getCellPadding = (cellSize: number): number => {
-    if (cellSize < 10) {
-        return 0;
-    }
+  if (cellSize < 10) {
+    return 0;
+  }
 
-    if (cellSize < 20) {
-        return 1;
-    }
+  if (cellSize < 20) {
+    return 1;
+  }
 
-    return 2;
+  return 2;
 };
 
 const useGrid = ({
-    canvas,
-    context,
-    cellSize,
-    showGrid,
-    colors: {
-        colorBackground,
-        colorGrid,
-        colorCell,
-    },
+  canvas,
+  context,
+  cellSize,
+  showGrid,
+  colors: { colorBackground, colorGrid, colorCell },
 }: GridParams): Grid => {
-    const gridSizeParams: GridSizeParams = getGridSizeParams(canvas, cellSize);
+  const gridSizeParams: GridSizeParams = getGridSizeParams(canvas, cellSize);
 
-    const renderGrid = (): void => {
-        context.fillStyle = colorBackground;
-        context.fillRect(0, 0, canvas.width, canvas.height);
+  const renderGrid = (): void => {
+    context.fillStyle = colorBackground;
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-        if (!showGrid) {
-            return;
-        }
+    if (!showGrid) {
+      return;
+    }
 
-        const {
-            xCellsCount,
-            yCellsCount,
-            gridWidth,
-            gridHeight,
-            offsetLeft,
-            offsetTop,
-        } = gridSizeParams;
+    const { xCellsCount, yCellsCount, gridWidth, gridHeight, offsetLeft, offsetTop } =
+      gridSizeParams;
 
-        context.strokeStyle = colorGrid;
+    context.strokeStyle = colorGrid;
 
-        context.beginPath();
+    context.beginPath();
 
-        for (let i = 0; i <= xCellsCount; i += 1) {
-            const xCoordinate = cellSize * i;
-            context.moveTo(xCoordinate + offsetLeft, offsetTop);
-            context.lineTo(xCoordinate + offsetLeft, gridHeight + offsetTop);
-        }
+    for (let i = 0; i <= xCellsCount; i += 1) {
+      const xCoordinate = cellSize * i;
+      context.moveTo(xCoordinate + offsetLeft, offsetTop);
+      context.lineTo(xCoordinate + offsetLeft, gridHeight + offsetTop);
+    }
 
-        for (let i = 0; i <= yCellsCount; i += 1) {
-            const yCoordinate = cellSize * i;
-            context.moveTo(offsetLeft, yCoordinate + offsetTop);
-            context.lineTo(gridWidth + offsetLeft, yCoordinate + offsetTop);
-        }
+    for (let i = 0; i <= yCellsCount; i += 1) {
+      const yCoordinate = cellSize * i;
+      context.moveTo(offsetLeft, yCoordinate + offsetTop);
+      context.lineTo(gridWidth + offsetLeft, yCoordinate + offsetTop);
+    }
 
-        context.stroke();
-    };
+    context.stroke();
+  };
 
-    const renderCell = (cell: Vector): void => {
-        const { offsetTop, offsetLeft } = gridSizeParams;
+  const renderCell = (cell: Vector): void => {
+    const { offsetTop, offsetLeft } = gridSizeParams;
 
-        const cellPadding = getCellPadding(cellSize);
-        const cellRenderingSize = cellSize - cellPadding * 2;
+    const cellPadding = getCellPadding(cellSize);
+    const cellRenderingSize = cellSize - cellPadding * 2;
 
-        const position: Vector = [
-            cell[0] * cellSize + offsetLeft + cellPadding,
-            cell[1] * cellSize + offsetTop + cellPadding,
-        ];
+    const position: Vector = [
+      cell[0] * cellSize + offsetLeft + cellPadding,
+      cell[1] * cellSize + offsetTop + cellPadding,
+    ];
 
-        context.fillStyle = colorCell;
-        context.fillRect(...position, cellRenderingSize, cellRenderingSize);
-    };
+    context.fillStyle = colorCell;
+    context.fillRect(...position, cellRenderingSize, cellRenderingSize);
+  };
 
-    const getCellByCoordinates: GetCellByCoordinates = (coordinates) => {
-        const coordinatesWithoutOffset = subtractVector(coordinates, [
-            gridSizeParams.offsetLeft,
-            gridSizeParams.offsetTop,
-        ]);
+  const getCellByCoordinates: GetCellByCoordinates = (coordinates) => {
+    const coordinatesWithoutOffset = subtractVector(coordinates, [
+      gridSizeParams.offsetLeft,
+      gridSizeParams.offsetTop,
+    ]);
 
-        if (coordinatesWithoutOffset[0] < 0 || coordinatesWithoutOffset[1] < 0) {
-            return undefined;
-        }
+    if (coordinatesWithoutOffset[0] < 0 || coordinatesWithoutOffset[1] < 0) {
+      return undefined;
+    }
 
-        const cell: Vector = [
-            Math.floor(coordinatesWithoutOffset[0] / cellSize),
-            Math.floor(coordinatesWithoutOffset[1] / cellSize),
-        ];
+    const cell: Vector = [
+      Math.floor(coordinatesWithoutOffset[0] / cellSize),
+      Math.floor(coordinatesWithoutOffset[1] / cellSize),
+    ];
 
-        if (cell[0] >= gridSizeParams.xCellsCount || cell[1] >= gridSizeParams.yCellsCount) {
-            return undefined;
-        }
+    if (cell[0] >= gridSizeParams.xCellsCount || cell[1] >= gridSizeParams.yCellsCount) {
+      return undefined;
+    }
 
-        return cell;
-    };
+    return cell;
+  };
 
-    return {
-        gridSizeParams,
-        renderGrid,
-        renderCell,
-        getCellByCoordinates,
-    };
+  return {
+    gridSizeParams,
+    renderGrid,
+    renderCell,
+    getCellByCoordinates,
+  };
 };
 
 export default useGrid;
